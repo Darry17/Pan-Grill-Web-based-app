82b152a62dbc7ba83563cef01b0c51bb278b5719
Darry Diaz <darry.a.diaz@gmail.com>
2025-06-27 18:26:49 +0800

    updated headers

---
+++ _headers.bash
@@ -0,0 +1,8 @@
+/*
+  Content-Security-Policy: 
+    default-src 'self';
+    script-src 'self';
+    style-src 'self' 'unsafe-inline';
+    style-src-elem 'self' https://fonts.googleapis.com;
+    font-src https://fonts.gstatic.com;
+    img-src 'self' data:;
